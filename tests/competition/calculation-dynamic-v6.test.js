dbe549a7853fff0f2e85e0ae032652128394586b
msphuongtien-blip <ms.phuongtien@gmail.com>
2026-09-05 16:37:00 -0500

    refactor tests: update competition module paths

--- tests/competition/calculation-dynamic-v6.test.js
+++ tests/competition/calculation-dynamic-v6.test.js
@@ -21,7 +21,7 @@ const vm = require('node:vm');
 
 const root = path.resolve(__dirname, '../..');
 const source = fs.readFileSync(
-    path.join(root, 'competition-calculation-v6.js'),
+    path.join(root, '../../modules/competition/competition-calculation-v6.js'),
     'utf8',
 );
 
@@ -30,7 +30,7 @@ const context = vm.createContext({
 });
 
 vm.runInContext(source, context, {
-    filename: 'competition-calculation-v6.js',
+    filename: '../../modules/competition/competition-calculation-v6.js',
 });
 
 const engine = context.CompetitionCalculationV6;
