3f202e277bb35e86e13d21097b612b140c69eef0
msphuongtien-blip <ms.phuongtien@gmail.com>
2026-09-07 11:10:51 -0500

    chore: bump V6 asset cache version

--- core/module-loader.js
+++ core/module-loader.js
@@ -11,7 +11,7 @@
  * - Bảo đảm module được nạp theo đúng thứ tự khai báo.
  */
 
-const V6_ASSET_VERSION = '20260907-competition-final-1';
+const V6_ASSET_VERSION = '20260907-competition-final-2';
 
 function loadApplicationModule(scriptId, source) {
     /*
