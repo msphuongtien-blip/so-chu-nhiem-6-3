5c24544dbe1f84fdbdc5d692bd212b586d4cb696
msphuongtien-blip <ms.phuongtien@gmail.com>
2026-09-07 18:01:51 -0500

    chore(competition): bump V6 asset version for week fix

--- core/module-loader.js
+++ core/module-loader.js
@@ -11,7 +11,7 @@
  * - Bảo đảm module được nạp theo đúng thứ tự khai báo.
  */
 
-const V6_ASSET_VERSION = '20260907-competition-final-2';
+const V6_ASSET_VERSION = '20260908-competition-week-fix-1';
 
 function loadApplicationModule(scriptId, source) {
     /*
